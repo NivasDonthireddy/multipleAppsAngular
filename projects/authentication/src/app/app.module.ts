3fc8c5f5f41501c2369b0d8d247d90b638886e43
Nivas Donthireddy <dlnivas@hotmail.com>
2023-02-04 19:38:23 +0530

    Login & Register Components of Authentication done

---
+++ projects/authentication/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, RouterModule],
+  providers: [CookieService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ projects/authentication/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  AUTHENTICATION_URL: 'http://localhost:4200',
+  ADMIN_URL: 'http://localhost:4201',
+  CUSTOMER_URL: 'http://localhost:4202',
+};
